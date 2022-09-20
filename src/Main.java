6938731a46eac212f33c59f759f404742bf1749a
Andrey_Rodionov <andrey_36rus@mail.ru>
2022-09-20 19:02:25 +0300

    задание 6,7,8

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,32 @@
 public class Main {
     public static void main(String[] args) {
-        // Задание 4
-        System.out.println("Задание 4");
-        var friend = 19;
-        System.out.println(friend);
-        friend += 2;
-        System.out.println(friend);
-        friend = friend / 7;
-        System.out.println(friend);
+        //Задание 6
+        System.out.println("Задание6");
 
-        //Задание 5
-        System.out.println("Задание 5");
-        var frog = 3.5;
-        System.out.println(frog);
-        frog = frog * 10;
-        System.out.println(frog);
-        frog = frog / 3.5;
-        System.out.println(frog);
-        frog += 4;
-        System.out.println(frog);
+        var BoxersWeight1 = 78.2;
+        var BoxersWeight2 = 82.7;
+        var totalWeight = BoxersWeight1 + BoxersWeight2;
+        System.out.println("Общий вес" + totalWeight + "кг");
+        var differenceInWeight = BoxersWeight2 - BoxersWeight1;
+        System.out.println("Разница в весе " + differenceInWeight + "кг");
+
+        //Задание 7
+        System.out.println("Задание 7");
+        var DifferenceWeight = BoxersWeight2 % BoxersWeight1;
+        System.out.println("Разница в весе через остаток от деления " + DifferenceWeight + "кг");
+
+        //Задание 8
+        System.out.println("Задание 8");
+        var totalWork = 640;
+        var worker1 = 8;
+        var totalEmployees = totalWork / worker1;
+        System.out.println("Всего работников в компании– " + totalEmployees + " человек");
+        var TotalEmployees = totalEmployees + 94;
+        var jobsForOne = totalWork * worker1;
+        System.out.println("Если в компании работает " + TotalEmployees + " человека, то всего " +jobsForOne + " часа работы может быть поделено между сотрудниками");
+
+
+
+
     }
 }
